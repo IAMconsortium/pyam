d1b5478c72893e58fd6f0fd8cc1d1da14cb68cd6
Daniel Huppmann <huppmann@iiasa.ac.at>
2017-08-14 17:01:40 +0200

    add unit tests for 'append()' function

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 from pyam_analysis import IamDataFrame
 from testing_utils import test_ia
+import pytest
 
 
 def test_model(test_ia):
@@ -15,3 +16,16 @@ def test_scenario(test_ia):
 def test_variable(test_ia):
     df = IamDataFrame(path='tests', file='testing_data', ext='csv')
     assert df.variables() == ['Primary Energy']
+
+
+def test_append(test_ia):
+    df = IamDataFrame(path='tests', file='testing_data', ext='csv')
+    df2 = df.append(path='tests', file='testing_data_2', ext='csv')
+    assert df.scenarios() == ['test_scenario']
+    assert df2.scenarios() == ['test_scenario', 'append_scenario']
+
+ 
+def test_append_duplicates(test_ia):
+    df = IamDataFrame(path='tests', file='testing_data', ext='csv')
+    pytest.raises(ValueError, df.append,
+                  path='tests', file='testing_data', ext='csv')
