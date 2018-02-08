ef6283aa6858df40287ee443b6ebbff27809d23a
Matthew Gidden <matthew.gidden@gmail.com>
2018-02-08 19:54:35 +0100

    add rc test

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -2,6 +2,8 @@ import pytest
 
 import matplotlib.pyplot as plt
 
+from pyam_analysis import plotting
+
 from testing_utils import plot_idf
 
 
@@ -52,4 +54,13 @@ def test_line_filter_title(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.filter({'variable': 'Primary Energy|Coal'}).line_plot(
         ax=ax, color='model', marker='scenario', legend=True)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_line_update_rc(plot_idf):
+    update = {'color': {'scenario': {'model1': 'cyan'}}}
+    plotting.run_control().update(update)
+    fig, ax = plt.subplots()
+    plot_idf.line_plot(ax=ax, color='model', legend=True)
     return fig
