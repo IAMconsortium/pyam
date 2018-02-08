5304f231dcc4a2c94217184104c8f41106634d7d
Matthew Gidden <matthew.gidden@gmail.com>
2018-02-08 20:00:58 +0100

    add baseline dir arg

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -4,52 +4,52 @@ import matplotlib.pyplot as plt
 
 from pyam_analysis import plotting
 
-from testing_utils import plot_idf
+from testing_utils import plot_idf, IMAGE_BASELINE_DIR
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_plot(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.line_plot(ax=ax)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_color(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.line_plot(ax=ax, color='model')
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_color_legend(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.line_plot(ax=ax, color='model', legend=True)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_marker_legend(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.line_plot(ax=ax, marker='model', legend=True)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_linestyle_legend(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.line_plot(ax=ax, linestyle='model', legend=True)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_single_color(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.line_plot(ax=ax, color='b', linestyle='model', legend=True)
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_filter_title(plot_idf):
     fig, ax = plt.subplots()
     plot_idf.filter({'variable': 'Primary Energy|Coal'}).line_plot(
@@ -57,7 +57,7 @@ def test_line_filter_title(plot_idf):
     return fig
 
 
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_BASELINE_DIR)
 def test_line_update_rc(plot_idf):
     update = {'color': {'model': {'test_model1': 'cyan'}}}
     plotting.run_control().update(update)
--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -6,6 +6,7 @@ from pyam_analysis import IamDataFrame
 here = os.path.dirname(os.path.realpath(__file__))
 data_path = os.path.join(here, 'testing_data.csv')
 plot_path = os.path.join(here, 'plot_data.csv')
+IMAGE_BASELINE_DIR = os.path.join(here, 'expected_figs')
 
 
 @pytest.fixture(scope="session")
