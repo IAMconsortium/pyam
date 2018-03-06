98910d77b88d86a657ab368640c952f37bdd5e71
Daniel Huppmann <dh@dergelbesalon.at>
2018-03-06 09:52:06 +0100

    fixing `validate()` function if both upper and lower check are given (#36)
    * add failing `validate_both()` test
    
    * fix `validate()` if both upper and lower bound are checked
    
    * fix formatting of test assertion
    
    * change pars of `test_validate_lo` to check for both upper and lower bound
    
    * add test that `exclude=False` really does not exclude
    
    * changing the order of syntax
    
    * add checking for list of scenarios in validation tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -138,11 +138,21 @@ def test_validate_up(meta_df):
     assert len(obs) == 1
     assert obs['year'].values[0] == 2010
 
+    assert list(meta_df['exclude']) == [False, False] # assert none excluded
+
 
 def test_validate_lo(meta_df):
-    obs = meta_df.validate({'Primary Energy': {'lo': 2.0}}, exclude=False)
+    obs = meta_df.validate({'Primary Energy': {'up': 8, 'lo': 2.0}})
     assert len(obs) == 1
     assert obs['year'].values[0] == 2005
+    assert list(obs['scenario'].values) == ['a_scenario']
+
+
+def test_validate_both(meta_df):
+    obs = meta_df.validate({'Primary Energy': {'up': 6.5, 'lo': 2.0}})
+    assert len(obs) == 2
+    assert list(obs['year'].values) == [2005, 2010]
+    assert list(obs['scenario'].values) == ['a_scenario', 'a_scenario2']
 
 
 def test_validate_year(meta_df):
