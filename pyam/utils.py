039bc005facc4e5ba3be5c6b4884e4fdc8644549
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-04-12 21:21:24 +1000

    Change to different check for database string

--- pyam/utils.py
+++ pyam/utils.py
@@ -132,7 +132,7 @@ def format_data(df):
     if 'notes' in df.columns:
         logger().info('Ignoring notes column in dataframe')
         df.drop(columns='notes', inplace=True)
-        df = df[~df.model.str.startswith('@')]
+        df = df[~df.model.str.lower().contains('database')]
 
     # check whether data in IAMC style or year/value layout
     if 'value' not in df.columns:
