4098b41fba4e82aa88e1e53e5fdd3c263636fccf
Daniel Huppmann <dh@dergelbesalon.at>
2018-06-29 15:17:22 +0200

    remove print

--- pyam/core.py
+++ pyam/core.py
@@ -977,8 +977,6 @@ def filter_by_meta(data, df, join_meta=False, **kwargs):
         by the given arguments (using `utils.pattern_match()`) and `col=None`
         joins the column without filtering
     """
-    print(data.columns)
-    print(META_IDX in list(data.columns))
     if not set(META_IDX).issubset(data.index.names + list(data.columns)):
         raise ValueError('missing required index dimensions or columns!')
 
