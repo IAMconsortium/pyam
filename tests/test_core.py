25f0be8ab577461e089f7b752033bf4958b3909c
Daniel Huppmann <huppmann@iiasa.ac.at>
2018-08-25 17:44:51 +0200

    add test for only adding columns (no filtering)

--- tests/test_core.py
+++ tests/test_core.py
@@ -629,6 +629,27 @@ def test_pd_filter_by_meta_nonmatching_index(meta_df):
     pd.testing.assert_frame_equal(obs, exp)
 
 
+def test_pd_join_by_meta_nonmatching_index(meta_df):
+    data = pd.DataFrame([
+        ['a_model', 'a_scenario3', 'a_region1', 1, 2],
+        ['a_model', 'a_scenario3', 'a_region2', 2, 3],
+        ['a_model', 'a_scenario2', 'a_region3', 3, 4],
+    ], columns=['model', 'scenario', 'region', 2010, 2020]
+    ).set_index(['model', 'region'])
+
+    meta_df.set_meta(['a', 'b'], 'string')
+
+    obs = filter_by_meta(data, meta_df, join_meta=True, string=None)
+    obs = obs.reindex(columns=['scenario', 2010, 2020, 'string'])
+
+    exp = data.copy()
+    exp['string'] = [None, None, 'b']
+
+    print(obs)
+    print(exp)
+
+    pd.testing.assert_frame_equal(obs, exp)
+
 def test_pd_filter_by_meta_no_index(meta_df):
     data = pd.DataFrame([
         ['a_model', 'a_scenario', 'a_region1', 1],
