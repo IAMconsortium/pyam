f7960b8bdb5445cc17c5224792ac9fa67cfa0619
Daniel Huppmann <huppmann@iiasa.ac.at>
2018-08-25 18:02:14 +0200

    move auxiliary df for unit test to top of file

--- tests/test_core.py
+++ tests/test_core.py
@@ -13,6 +13,13 @@ from pyam.core import _meta_idx
 from conftest import TEST_DATA_DIR
 
 
+df_filter_by_meta_matching_idx = pd.DataFrame([
+    ['a_model', 'a_scenario', 'a_region1', 1],
+    ['a_model', 'a_scenario', 'a_region2', 2],
+    ['a_model', 'a_scenario2', 'a_region3', 3],
+], columns=['model', 'scenario', 'region', 'col'])
+
+
 def test_get_item(test_df):
     assert test_df['model'].unique() == ['a_model']
 
@@ -589,12 +596,7 @@ def test_convert_unit():
 
 
 def test_pd_filter_by_meta(meta_df):
-    data = pd.DataFrame([
-        ['a_model', 'a_scenario', 'a_region1', 1],
-        ['a_model', 'a_scenario', 'a_region2', 2],
-        ['a_model', 'a_scenario2', 'a_region3', 3],
-    ], columns=['model', 'scenario', 'region', 'col']
-    ).set_index(['model', 'region'])
+    data = df_filter_by_meta_matching_idx.set_index(['model', 'region'])
 
     meta_df.set_meta([True, False], 'boolean')
     meta_df.set_meta(0, 'integer')
@@ -630,13 +632,7 @@ def test_pd_filter_by_meta_nonmatching_index(meta_df):
 
 
 def test_pd_join_by_meta_nonmatching_index(meta_df):
-    data = pd.DataFrame([
-        ['a_model', 'a_scenario3', 'a_region1', 1, 2],
-        ['a_model', 'a_scenario3', 'a_region2', 2, 3],
-        ['a_model', 'a_scenario2', 'a_region3', 3, 4],
-    ], columns=['model', 'scenario', 'region', 2010, 2020]
-    ).set_index(['model', 'region'])
-
+    data = df_filter_by_meta_nonmatching_idx
     meta_df.set_meta(['a', 'b'], 'string')
 
     obs = filter_by_meta(data, meta_df, join_meta=True, string=None)
