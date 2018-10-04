ac7fcd4e0fadf3a990363b1d6b4582db0580643b
Daniel Huppmann <huppmann@iiasa.ac.at>
2018-10-04 16:43:31 +0200

    add test for empty returned list of `cross_threshold()`

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -10,3 +10,9 @@ def test_cross_treshold():
     y = pd.Series(data=[np.nan, 1, 3, 1], index=[2002, 2005, 2007, 2013])
     obs = pyam.cross_threshold(y, 2)
     assert obs == [2007, 2011]
+
+
+def test_cross_treshold_empty():
+    y = pd.Series(data=[np.nan, 1, 3, 1], index=[2002, 2005, 2007, 2013])
+    obs = pyam.cross_threshold(y, 4)
+    assert obs == []
