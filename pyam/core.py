b5ad6f181180de343e4409bc15dce36222dd8f00
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-09 18:20:32 +0200

    Update docstrings

--- pyam/core.py
+++ pyam/core.py
@@ -542,15 +542,15 @@ class IamDataFrame(object):
     def check_aggregate_regions(self, variable, region='World',
                                 components=None, units=None,
                                 exclude_on_fail=False, **kwargs):
-        """Check whether the world timeseries data match the aggregation
-        of regions
+        """Check whether the region timeseries data match the aggregation
+        of components
 
         Parameters
         ----------
         variable: str
-            variable to be checked for matching aggregation of regional data
+            variable to be checked for matching aggregation of components data
         region: str
-            region to be checked for matching aggregation of regional data
+            region to be checked for matching aggregation of components data
         components: list of str, default None
             list of regions, defaults to all regions except region
         units: str or list of str, default None
@@ -625,8 +625,21 @@ class IamDataFrame(object):
             return diff.unstack().rename_axis(None, axis=1)
 
     def check_internal_consistency(self, **kwargs):
-        # TODO: test this and write docstring, kwargs passed to np is close
-        # need to have a aggregate_regions keyword argument
+        """Check whether the database is internally consistent
+
+        We check that all variables are equal to the sum of their sectoral components
+        and that all the regions add up to the World total. If the check is passed,
+        None is returned, otherwise a dictionary of inconsistent variables is returned.
+
+        Note: at the moment, this method's regional checking is limited to checking
+        that all the regions sum to the World region. We cannot make this more
+        automatic unless we start to store how the regions relate, see [this
+        issue](https://github.com/IAMconsortium/pyam/issues/106).
+
+        Parameters
+        ----------
+        kwargs: passed to `np.isclose()`
+        """
         inconsistent_vars = {}
         for variable in self.variables():
             diff_agg = self.check_aggregate(variable, **kwargs)
@@ -638,7 +651,6 @@ class IamDataFrame(object):
                 inconsistent_vars[variable + "-regional"] = diff_regional
 
         return inconsistent_vars if inconsistent_vars else None
-
 
     def _exclude_on_fail(self, df):
         """Assign a selection of scenarios as `exclude: True` in meta"""
