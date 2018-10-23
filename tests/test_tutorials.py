3c5a6d13af8d7b409c6e6441c5883d33c0f87487
Matthew Gidden <matthew.gidden@gmail.com>
2018-10-23 11:26:39 +0200

    now make tutorial tests dependent on pandoc install

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -13,6 +13,15 @@ try:
 except:
     jupyter_installed = False
 
+jupyter_reason = 'requires Jupyter Notebook to be installed'
+
+try:
+    pandoc_installed = subprocess.call(['which', 'pandoc']) == 0
+except:
+    pandoc_installed = False
+
+pandoc_reason = 'requires Pandoc to be installed'
+
 tut_path = os.path.join(here, '..', 'doc', 'source', 'tutorials')
 
 # taken from the execellent example here:
@@ -48,24 +57,24 @@ def _notebook_run(path, kernel=None, capsys=None):
     return nb, errors
 
 
-@pytest.mark.skipif(not jupyter_installed,
-                    reason='requires Jupyter Notebook to be installed')
+@pytest.mark.skipif(not jupyter_installed, reason=jupyter_reason)
+@pytest.mark.skipif(not pandoc_installed, reason=pandoc_reason)
 def test_pyam_first_steps(capsys):
     fname = os.path.join(tut_path, 'pyam_first_steps.ipynb')
     nb, errors = _notebook_run(fname, capsys=capsys)
     assert errors == []
 
 
-@pytest.mark.skipif(not jupyter_installed,
-                    reason='requires Jupyter Notebook to be installed')
+@pytest.mark.skipif(not jupyter_installed, reason=jupyter_reason)
+@pytest.mark.skipif(not pandoc_installed, reason=pandoc_reason)
 def test_checking_databases():
     fname = os.path.join(tut_path, 'checking_databases.ipynb')
     nb, errors = _notebook_run(fname)
     assert errors == []
 
 
-@pytest.mark.skipif(not jupyter_installed,
-                    reason='requires Jupyter Notebook to be installed')
+@pytest.mark.skipif(not jupyter_installed, reason=jupyter_reason)
+@pytest.mark.skipif(not pandoc_installed, reason=pandoc_reason)
 def test_pyam_logo():
     fname = os.path.join(tut_path, 'pyam_logo.ipynb')
     nb, errors = _notebook_run(fname)
