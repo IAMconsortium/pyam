5e301f1c1997b3f3f1cb07f8fbd8be88f57f6ac3
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-24 16:20:48 +0200

    Update install setup

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ from __future__ import print_function
 import glob
 import os
 import shutil
+from subprocess import call
 
 from setuptools import setup, Command, find_packages
 from setuptools.command.install import install
 
 # Thanks to http://patorjk.com/software/taag/
 logo = r"""
- ______   __  __     ______     __    __                                             
-/\  == \ /\ \_\ \   /\  __ \   /\ "-./  \                                            
-\ \  _-/ \ \____ \  \ \  __ \  \ \ \-./\ \                                           
- \ \_\    \/\_____\  \ \_\ \_\  \ \_\ \ \_\                                          
-  \/_/     \/_____/   \/_/\/_/   \/_/  \/_/                                          
+ ______   __  __     ______     __    __
+/\  == \ /\ \_\ \   /\  __ \   /\ "-./  \
+\ \  _-/ \ \____ \  \ \  __ \  \ \ \-./\ \
+ \ \_\    \/\_____\  \ \_\ \_\  \ \_\ \ \_\
+  \/_/     \/_____/   \/_/\/_/   \/_/  \/_/
 """
 
 INFO = {
@@ -43,6 +44,24 @@ class Cmd(install):
                 shutil.rmtree(d)
 
 
+# thank you https://stormpath.com/blog/building-simple-cli-interfaces-in-python
+class RunTests(Command):
+    """Run all tests."""
+    description = 'run tests'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run all tests!"""
+        errno = call(['py.test', '--cov=skele', '--cov-report=term-missing'])
+        raise SystemExit(errno)
+
+
 def main():
     print(logo)
 
@@ -63,6 +82,20 @@ def main():
     cmdclass = {
         'install': Cmd,
     }
+    install_requirements=[
+        "argparse",
+        "numpy",
+        "pandas >=0.21.0",
+        "pyyaml",
+        "xlrd",
+        "xlsxwriter",
+        "matplotlib",
+        "seaborn",
+        "six",
+    ]
+    extra_requirements = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    }
     setup_kwargs = {
         "name": "pyam-iamc",
         "version": INFO['version'],
@@ -75,6 +108,8 @@ def main():
         "entry_points": entry_points,
         "cmdclass": cmdclass,
         "package_data": package_data,
+        "install_requires": install_requirements,
+        "extras_require": extra_requirements,
     }
     rtn = setup(**setup_kwargs)
 
