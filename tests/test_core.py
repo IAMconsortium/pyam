a76544c9d816c63a270aa11ad27eb06abbdaea8c
Matthew Gidden <matthew.gidden@gmail.com>
2018-12-05 16:54:18 +0100

    fix function name in test

--- tests/test_core.py
+++ tests/test_core.py
@@ -58,7 +58,7 @@ def test_to_excel(test_df):
 
 def test_to_csv(test_df):
     fname = 'foo_testing.csv'
-    test_df.to_excel(fname)
+    test_df.to_csv(fname)
     assert os.path.exists(fname)
     os.remove(fname)
 
