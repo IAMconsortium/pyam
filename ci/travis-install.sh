d46503e52df4651f6ba833ecac6bf8afb4d383af
Matthew Gidden <matthew.gidden@gmail.com>
2019-01-19 06:12:51 -0600

    last chance for windows

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -41,6 +41,8 @@ conda install -n testing --yes \
 
 # these have to be installed from conda-forge to get right gdal packages
 conda install -n testing -c conda-forge --yes \
+      cython \
+      pyproj \
       libkml \
       gdal \
       fiona \
