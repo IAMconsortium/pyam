355d22e1ba9b6a32d63fe5d74e35ca87c98d8991
Matthew Gidden <matthew.gidden@gmail.com>
2019-01-25 10:08:09 +0100

    clean up the logic a bit of column discovery

--- pyam/utils.py
+++ pyam/utils.py
@@ -6,7 +6,7 @@ import re
 import glob
 import collections
 import datetime
-from dateutil import parser
+import dateutil
 import time
 
 import numpy as np
@@ -171,16 +171,14 @@ def format_data(df):
         year_cols, time_cols, extra_cols = [], [], []
         for i in cols:
             try:
-                year_cols.append(i) if int(i) else None
+                int(i)  # this is a year
+                year_cols.append(i)
             except (ValueError, TypeError):
-                if isinstance(i, datetime.datetime):
-                    time_cols.append(i)
-                    continue
                 try:
-                    parser.parse(i)
+                    dateutil.parser.parse(str(i))  # this is datetime
                     time_cols.append(i)
                 except ValueError:
-                    extra_cols.append(i)
+                    extra_cols.append(i)  # some other string
         if year_cols and not time_cols:
             time_col = 'year'
             melt_cols = year_cols
