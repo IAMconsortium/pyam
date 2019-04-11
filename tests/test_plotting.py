4f94e310b0b03e981f633529046129b5645ca381
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-04-11 16:18:59 +1000

    Appease stickler

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -571,7 +571,7 @@ def test_stack_plot_negative_emissions_with_total(plot_stack_plot_df):
 
 
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
-def test_stack_plot_negative_emissions_kwargs_default_total(plot_stack_plot_df):
+def test_stack_plot_negative_emissions_kwargs_def_total(plot_stack_plot_df):
     fig, ax = plt.subplots(figsize=(8, 8))
     plot_stack_plot_df.stack_plot(
         alpha=0.5,
