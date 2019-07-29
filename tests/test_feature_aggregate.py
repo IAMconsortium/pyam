8785015c565f3f61e58bd73e62d0849e55d57adc
Peter Kolp <peter.kolp.wien@gmail.com>
2019-07-29 16:16:34 +0200

    add unit test unknow method

--- tests/test_feature_aggregate.py
+++ tests/test_feature_aggregate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pytest
 from pyam import check_aggregate, IAMC_IDX
 
 from conftest import TEST_DTS
@@ -24,6 +25,11 @@ def test_do_aggregate_append(meta_df):
         exp.columns = pd.to_datetime(exp.columns)
 
     pd.testing.assert_frame_equal(obs, exp)
+
+
+def test_aggregate_unknown_method(reg_df):
+    pytest.raises(ValueError, reg_df.aggregate_region, 'Primary Energy',
+                  method='foo')
 
 
 def test_check_aggregate_pass(check_aggregate_df):
