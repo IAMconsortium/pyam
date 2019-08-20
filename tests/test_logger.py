e50ddf61475ede1f03457e9a34df77ed70159a8f
Nikolay Kushin <zikolach@gmail.com>
2019-08-20 14:41:21 +0200

    Fix adjusting log level called with uninitialized logger (#263)
    * Fix adjusting log level called with uninitialized logger
    * Add test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,5 +1,6 @@
-from pyam import logger
-from pyam.logger import adjust_log_level
+import sys
+
+from pyam.logger import logger, adjust_log_level
 
 
 def test_context_adjust_log_level():
@@ -7,3 +8,11 @@ def test_context_adjust_log_level():
     with adjust_log_level():
         assert logger().getEffectiveLevel() == 40
     assert logger().getEffectiveLevel() == 20
+
+
+def test_adjusting_level_for_not_initialized_logger():
+    # de-initialize logger to simulate adjust_log_level called before logger
+    pyam_logger = sys.modules['pyam.logger']
+    pyam_logger._LOGGER = None
+    with adjust_log_level():
+        pass
