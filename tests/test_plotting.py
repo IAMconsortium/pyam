b10847a0185c55dd7c80cf0f3e8cda2441ce532a
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-09-30 21:38:49 +1000

    Appease stickler

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -585,10 +585,10 @@ def test_stack_plot_negative_emissions_kwargs_custom_total(plot_stack_plot_df):
 @pytest.mark.mpl_image_compare(**MPL_KWARGS)
 def test_stack_plot_missing_zero_issue_266(plot_stack_plot_df):
     df = pyam.IamDataFrame(pd.DataFrame([
-        ['a', 1, 2, 3, 4],
-        ['b', 0, 1, 2, 3],
-        ['c', -1, 1, -1, -1],
-        ['d', 1, 1, 1, -1]
+            ['a', 1, 2, 3, 4],
+            ['b', 0, 1, 2, 3],
+            ['c', -1, 1, -1, -1],
+            ['d', 1, 1, 1, -1]
         ],
         columns=['variable', 2010, 2020, 2030, 2040],
     ), model='model_a', scenario='scen_a', region='World', unit='some_unit')
