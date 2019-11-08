c13a89df7c1ca75dc7d0b5d53819e5cb3741667a
Daniel Huppmann <dh@dergelbesalon.at>
2019-11-08 14:03:23 +0100

    migrate merging of `meta` in `append()` to own auxiliary function

--- pyam/utils.py
+++ pyam/utils.py
@@ -268,6 +268,39 @@ def sort_data(data, cols):
     return data.sort_values(cols)[cols + ['value']].reset_index(drop=True)
 
 
+def merge_meta(ret, other, ignore_meta_conflict):
+    """Merge the ``meta`` tables of two ``IamDataFrames; raise error if values are in conflict (optional)"""
+    diff = other.meta.index.difference(ret.meta.index)
+    intersect = other.meta.index.intersection(ret.meta.index)
+
+    # merge other.meta columns not in self.meta for existing scenarios
+    if not intersect.empty:
+        # if not ignored, check that overlapping meta dataframes are equal
+        if not ignore_meta_conflict:
+            cols = [i for i in other.meta.columns if i in ret.meta.columns]
+            if not ret.meta.loc[intersect, cols].equals(
+                    other.meta.loc[intersect, cols]):
+                conflict_idx = (
+                    pd.concat([ret.meta.loc[intersect, cols],
+                               other.meta.loc[intersect, cols]]
+                              ).drop_duplicates()
+                        .index.drop_duplicates()
+                )
+                msg = 'conflict in `meta` for scenarios {}'.format(
+                    [i for i in pd.DataFrame(index=conflict_idx).index])
+                raise ValueError(msg)
+
+        cols = [i for i in other.meta.columns if i not in ret.meta.columns]
+        _meta = other.meta.loc[intersect, cols]
+        ret.meta = ret.meta.merge(_meta, how='outer',
+                                  left_index=True, right_index=True)
+
+    # join other.meta for new scenarios
+    if not diff.empty:
+        sort_kwarg = {} if int(pd.__version__.split('.')[1]) < 23 \
+            else dict(sort=False)
+        ret.meta = ret.meta.append(other.meta.loc[diff, :], **sort_kwarg)
+
 def find_depth(data, s='', level=None):
     """
     return or assert the depth (number of `|`) of variables
