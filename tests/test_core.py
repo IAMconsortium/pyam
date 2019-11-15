0ec2a22e4419180f825c6cd513e6e39b6171e879
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-11-15 23:17:19 +1100

    Add failing test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import pytest
 import re
 import datetime
@@ -134,6 +135,19 @@ def test_init_datetime_subclass_long_timespan(test_pd_df):
 
     assert df["time"].max() == tmax
     assert df["time"].min() == tmin
+
+
+def test_init_dropping_nans_message(test_pd_df, caplog):
+    tdf = test_pd_df.copy()
+    tdf["extra_col"] = "test_val"
+    tdf["extra_col"][tdf["variable"] == "Primary Energy|Coal"] = np.nan
+
+    res = IamDataFrame(tdf)
+    assert "Primary Energy|Coal" not in res.variables().tolist()
+
+    drop_message = "dropping na rows"
+    message_idx = caplog.messages.index[drop_message]
+    assert caplog.records[message_idx].levelno == logging.WARNING
 
 
 def test_to_excel(test_df):
