16c0ea5714cbb85bbbfebda895b2a853cd1b7dc4
Zeb Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-11-27 02:22:41 +1100

    fix logging in notebooks, make default logging more verbose (#294)
    * Add first pass at notebook logging
    
    * Appease stickler

--- pyam/__init__.py
+++ pyam/__init__.py
@@ -9,21 +9,30 @@ from pyam.logging import *
 from pyam.run_control import *
 from pyam.iiasa import read_iiasa  # noqa: F401
 
-# in Jupyter notebooks: disable autoscroll, activate warnings
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+# in Jupyter notebooks: disable autoscroll and set logger to info
 try:
     get_ipython().run_cell_magic(u'javascript', u'',
                                  u'IPython.OutputArea.prototype._should_scroll = function(lines) { return false; }')
-    warnings.simplefilter('default')
 
-    def custom_formatwarning(msg, category, filename, lineno, line=''):
-        # ignore everything except the message
-        return str(msg) + '\n'
-    warnings.formatwarning = custom_formatwarning
+    logger.setLevel(logging.INFO)
+
+    stderr_info_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(name)s - %(levelname)s: %(message)s')
+    stderr_info_handler.setFormatter(formatter)
+    logger.addHandler(stderr_info_handler)
+
+    log_msg = (
+        "Running in a notebook, adding stderr handler and setting "
+        "`{}` logging level to `logging.INFO`".format(__name__)
+    )
+    logger.info(log_msg)
+
 except Exception:
     pass
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
