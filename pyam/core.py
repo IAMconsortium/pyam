ade3dbad2694540d0760b31b34d09e631cdbf7f6
Rlamboll <rlamboll@hotmail.co.uk>
2020-02-17 14:08:19 +0000

    Update pyam/core.py
    Improving error message
    
    Co-Authored-By: Daniel Huppmann <dh@dergelbesalon.at>

--- pyam/core.py
+++ pyam/core.py
@@ -359,7 +359,7 @@ class IamDataFrame(object):
             df = df[np.isnan(df[time])]
         if self.time_col == "year":
             if not isinstance(time, int):
-                raise ValueError("the year value, {} is not an integer".format(time))
+                raise ValueError("the `time` argument `{}` is not an integer".format(time))
             fill_values = df.apply(fill_series, raw=False, axis=1, time=time)
         else:
             fill_values = df.apply(fill_series, raw=False, axis=1, time=time)
