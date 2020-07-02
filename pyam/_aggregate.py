26a924451555a26fead06a0d5a14f6222332eee0
Thorsten Burandt <2787226+tburandt@users.noreply.github.com>
2020-07-02 09:44:26 +0200

    Update pyam/_aggregate.py
    Co-authored-by: Daniel Huppmann <dh@dergelbesalon.at>

--- pyam/_aggregate.py
+++ pyam/_aggregate.py
@@ -58,8 +58,7 @@ def _aggregate_recursive(df, variable, method=np.sum):
     _df_aggregated = None
     _df = df.copy()
 
-
-    # iterate over variables to find all subcategories that need to be aggregated
+    # iterate over variables to find all subcategories to be aggregated
     sub_variables = []
     for d in reversed(range(1, max(find_depth(df.data.variable)) + 1)):
         depth = find_depth(df.data.variable)
