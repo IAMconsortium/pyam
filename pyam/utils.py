88f987e0449304890c9df6199eea5909a93a13b3
Daniel Huppmann <dh@dergelbesalon.at>
2020-08-02 19:56:30 +0200

    remove functions (moved to index.py)

--- pyam/utils.py
+++ pyam/utils.py
@@ -520,25 +520,3 @@ def reduce_hierarchy(x, depth):
     _x = x.split('|')
     depth = len(_x) + depth - 1 if depth < 0 else depth
     return '|'.join(_x[0:(depth + 1)])
-
-
-def get_index_levels(df, level):
-    """Return the levels for a specific level"""
-    for i, n in enumerate(df.index.names):
-        if n == level:
-            return list(df.index.levels[i])
-
-    raise ValueError(f'This object does not have an index level `{level}`!')
-
-
-def replace_index_value(df, level, current, to, verify_integrity=True):
-    """Replaces a value in a particular index level"""
-    levels = []
-    for n, l in zip(df.index.names, df.index.levels):
-        if n == level:
-            levels.append([to if i == current else i for i in l])
-        else:
-            levels.append(l)
-
-    df.index = pd.MultiIndex(codes=df.index.codes, names=df.index.names,
-                             levels=levels, verify_integrity=verify_integrity)
