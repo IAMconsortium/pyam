37f9f913284c2219d21c73f26da435da5cfa3a5b
Daniel Huppmann <dh@dergelbesalon.at>
2020-09-28 17:18:11 +0200

    refactor all test for `validate()` to explicitly check the return type

--- tests/test_feature_validation.py
+++ tests/test_feature_validation.py
@@ -58,93 +58,83 @@ def test_require_variable_year_list(test_df):
     pdt.assert_frame_equal(obs, exp)
 
 
-def test_validate_all_pass(test_df):
-    obs = test_df.validate(
-        {'Primary Energy': {'up': 10}}, exclude_on_fail=True)
+def test_validate_pass(test_df):
+    obs = test_df.validate({'Primary Energy': {'up': 10}},
+                           exclude_on_fail=True)
     assert obs is None
-    assert len(test_df.data) == 6  # data unchanged
-
     assert list(test_df['exclude']) == [False, False]  # none excluded
 
 
 def test_validate_nonexisting(test_df):
+    # checking that a scenario with no relevant value does not fail validation
     obs = test_df.validate({'Primary Energy|Coal': {'up': 2}},
                            exclude_on_fail=True)
-    assert len(obs) == 1
-    assert obs.index.get_level_values('scenario').values[0] == 'scen_a'
-
-    assert list(test_df['exclude']) == [True, False]  # scenario with failed
-    # validation excluded, scenario with non-defined value passes validation
+    # checking that the return-type is correct
+    pdt.assert_frame_equal(obs, test_df.data[3:4].reset_index(drop=True))
+    # scenario with failed validation excluded, scenario with no value passes
+    assert list(test_df['exclude']) == [True, False]
 
 
 def test_validate_up(test_df):
-    obs = test_df.validate({'Primary Energy': {'up': 6.5}},
-                           exclude_on_fail=False)
-    assert len(obs) == 1
-    if 'year' in test_df.data:
-        assert obs.index.get_level_values('year').values[0] == 2010
-    else:
-        exp_time = pd.to_datetime(datetime(2010, 7, 21))
-        assert pd.to_datetime(obs.index.get_level_values('time')
-                              .values[0]).date() == exp_time
+    # checking that the return-type is correct
+    obs = test_df.validate({'Primary Energy': {'up': 6.5}})
+    pdt.assert_frame_equal(obs, test_df.data[5:6].reset_index(drop=True))
+    assert list(test_df['exclude']) == [False, False]
 
-    assert list(test_df['exclude']) == [False, False]  # assert none excluded
+    # checking exclude on fail
+    obs = test_df.validate({'Primary Energy': {'up': 6.5}},
+                           exclude_on_fail=True)
+    pdt.assert_frame_equal(obs, test_df.data[5:6].reset_index(drop=True))
+    assert list(test_df['exclude']) == [False, True]
 
 
 def test_validate_lo(test_df):
-    obs = test_df.validate({'Primary Energy': {'up': 8, 'lo': 2.0}})
-    assert len(obs) == 1
-    if 'year' in test_df.data:
-        assert obs.index.get_level_values('year').values[0] == 2005
-    else:
-        exp_year = pd.to_datetime(datetime(2005, 6, 17))
-        assert pd.to_datetime(obs.index.get_level_values('time')
-                              .values[0]).date() == exp_year
+    # checking that the return-type is correct
+    obs = test_df.validate({'Primary Energy': {'up': 8, 'lo': 2}})
+    pdt.assert_frame_equal(obs, test_df.data[0:1].reset_index(drop=True))
+    assert list(test_df['exclude']) == [False, False]
 
-    assert list(obs.index.get_level_values('scenario').values) == ['scen_a']
+    # checking exclude on fail
+    obs = test_df.validate({'Primary Energy': {'up': 8, 'lo': 2}},
+                           exclude_on_fail=True)
+    pdt.assert_frame_equal(obs, test_df.data[0:1].reset_index(drop=True))
+    assert list(test_df['exclude']) == [True, False]
 
 
 def test_validate_both(test_df):
-    obs = test_df.validate({'Primary Energy': {'up': 6.5, 'lo': 2.0}})
-    assert len(obs) == 2
-    if 'year' in test_df.data:
-        assert list(obs.index.get_level_values('year').values) == [2005, 2010]
-    else:
-        exp_time = pd.to_datetime(TEST_DTS)
-        obs.index.set_levels(obs.index.get_level_values('time')
-                                .normalize(), level=5)
-        assert (pd.to_datetime(obs.index.get_level_values('time').values)
-                .date == exp_time).all()
+    # checking that the return-type is correct
+    obs = test_df.validate({'Primary Energy': {'up': 6.5, 'lo': 2}})
+    pdt.assert_frame_equal(obs, test_df.data[0:6:5].reset_index(drop=True))
+    assert list(test_df['exclude']) == [False, False]
 
-    exp = ['scen_a', 'scen_b']
-    assert list(obs.index.get_level_values('scenario').values) == exp
+    # checking exclude on fail
+    obs = test_df.validate({'Primary Energy': {'up': 6.5, 'lo': 2}},
+                           exclude_on_fail=True)
+    pdt.assert_frame_equal(obs, test_df.data[0:6:5].reset_index(drop=True))
+    assert list(test_df['exclude']) == [True, True]
 
 
 def test_validate_year(test_df):
-    obs = test_df.validate({'Primary Energy': {'up': 5.0, 'year': 2005}},
-                           exclude_on_fail=False)
+    # checking that the year filter works as expected
+    obs = test_df.validate({'Primary Energy': {'up': 6, 'year': 2005}})
     assert obs is None
 
-    obs = test_df.validate({'Primary Energy': {'up': 5.0, 'year': 2010}},
-                           exclude_on_fail=False)
-    assert len(obs) == 2
+    # checking that the return-type is correct
+    obs = test_df.validate({'Primary Energy': {'up': 6, 'year': 2010}})
+    pdt.assert_frame_equal(obs, test_df.data[5:6].reset_index(drop=True))
+    assert list(test_df['exclude']) == [False, False]
 
-
-def test_validate_exclude(test_df):
-    test_df.validate({'Primary Energy': {'up': 6.0}}, exclude_on_fail=True)
+    # checking exclude on fail
+    obs = test_df.validate({'Primary Energy': {'up': 6, 'year': 2010}},
+                           exclude_on_fail=True)
+    pdt.assert_frame_equal(obs, test_df.data[5:6].reset_index(drop=True))
     assert list(test_df['exclude']) == [False, True]
 
 
 def test_validate_top_level(test_df):
-    obs = validate(test_df, criteria={'Primary Energy': {'up': 6.0}},
+    obs = validate(test_df, criteria={'Primary Energy': {'up': 6}},
                    exclude_on_fail=True, variable='Primary Energy')
-    assert len(obs) == 1
-    if 'year' in test_df._data.index.names:
-        assert obs.index.get_level_values('year').values[0] == 2010
-    else:
-        exp_time = datetime(2010, 7, 21).date()
-        obs_time = obs.index.get_level_values('time')[0].date()
-        assert exp_time == obs_time
+    pdt.assert_frame_equal(obs, test_df.data[5:6].reset_index(drop=True))
     assert list(test_df['exclude']) == [False, True]
 
 
