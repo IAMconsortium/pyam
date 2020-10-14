149a69beaf5a0e6daa7ec137e51bf7047a87094a
Daniel Huppmann <dh@dergelbesalon.at>
2020-10-14 09:50:57 +0200

    try making the layout nicer

--- doc/source/_examples/plot_timeseries.py
+++ doc/source/_examples/plot_timeseries.py
@@ -8,15 +8,22 @@ import matplotlib.pyplot as plt
 import pyam
 
 ###############################
-# Read in the data from the first-steps tutorial and show a summary.
+# Read in the data from the first-steps tutorial and show a summary
+# *****************************************************************
+#
 # If you haven't cloned the **pyam** GitHub repository to your machine,
-# you can download the file from https://github.com/IAMconsortium/pyam/tree/master/doc/source/tutorials
+# you can download the file
+# from https://github.com/IAMconsortium/pyam/tree/master/doc/source/tutorials
+#
 # Make sure to place the file in the same folder as this script/notebook.
 
 df = pyam.IamDataFrame('tutorial_data.csv')
 df
 
 ###############################
+# A simple line chart
+# *******************
+#
 # We show a simple line chart of the regional components
 # of CO2 emissions for one scenario.
 
@@ -27,5 +34,5 @@ data = (
 )
 
 fig, ax = plt.subplots()
-data.line_plot(ax=ax, color='region')
+data.line_plot(ax=ax, color='region', title='CO2 emissions by region')
 plt.show()
