e3453480ffacb12bfdf83f193819076023810312
mabudz <maik.budzinski@ntnu.no>
2020-12-15 12:39:01 +0100

    PEP 8

--- pyam/figures.py
+++ pyam/figures.py
@@ -6,9 +6,10 @@ from pyam.index import get_index_levels
 
 logger = logging.getLogger(__name__)
 
+
 def sankey(df, mapping):
     """Plot sankey diagram of existing data using plotly.
-    
+
         Currently only for one year possible.
         Parameters
         ----------
@@ -28,12 +29,12 @@ def sankey(df, mapping):
     if len(df.columns) != 1:
         msg = 'Can only plot one year. Filter before!'
         raise ValueError(msg)
-    
+
     mapping_df = pd.DataFrame.from_dict(mapping, orient='index',
                                         columns=['source', 'target'])
     mapping_df_merged = mapping_df.merge(df, how='left', left_index=True,
                                          right_on='variable')
-    label_set = set(mapping_df_merged['source']. \
+    label_set = set(mapping_df_merged['source'].
                     append(mapping_df_merged['target']))
     label_series = pd.Series(list(label_set))
     for ind, val in label_series.items():
@@ -42,21 +43,21 @@ def sankey(df, mapping):
     unit = get_index_levels(df, 'unit')[0]
     year = df.columns[0]
     fig = go.Figure(data=[go.Sankey(
-        valuesuffix = unit,
-        node = dict(
-            pad = 15,
-            thickness = 10,
-            line = dict(color = "black", width = 0.5),
-            label = label_series,
+        valuesuffix=unit,
+        node=dict(
+            pad=15,
+            thickness=10,
+            line=dict(color="black", width=0.5),
+            label=label_series,
             hovertemplate='%{label}: %{value}<extra></extra>',
-            color = "blue"
-            ),
-        link = dict(
-            source = mapping_df_merged.source,
-            target = mapping_df_merged.target,
-            value = mapping_df_merged[year],
+            color="blue"
+        ),
+        link=dict(
+            source=mapping_df_merged.source,
+            target=mapping_df_merged.target,
+            value=mapping_df_merged[year],
             hovertemplate='"%{source.label}" to "%{target.label}": \
                 %{value}<extra></extra>'
-            ))])
+    ))])
     fig.update_layout(title_text="%s %s" % (region, year), font_size=10)
     return fig
