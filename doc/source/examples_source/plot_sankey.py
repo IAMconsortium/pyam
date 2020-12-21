2340ba3c259fa28c91c203cf4690bcce0bc1899b
Daniel Huppmann <dh@dergelbesalon.at>
2020-12-21 18:55:12 +0100

    add sankey gallery example

---
+++ doc/source/examples_source/plot_sankey.py
@@ -0,0 +1,52 @@
+"""
+==============
+Sankey diagram
+==============
+
+"""
+###############################
+# Read in example data and show a summary
+# ***************************************
+#
+# This gallery uses a small selection of the data
+# compiled for the IPCC's *Special Report on Global Warming of 1.5°C* ([SR15](http://ipcc.ch/sr15/)).
+# The complete scenario ensemble data is publicly available from the
+# [IAMC 1.5°C Scenario Explorer and Data hosted by IIASA](https://data.ene.iiasa.ac.at/iamc-1.5c-explorer).
+#
+# Please read the [License](https://data.ene.iiasa.ac.at/iamc-1.5c-explorer/#/license) page
+# of the IAMC 1.5°C Scenario Explorer before using the full scenario data for scientific analyis or other work.
+#
+# If you haven't cloned the **pyam** GitHub repository to your machine,
+# you can download the file from
+# https://github.com/IAMconsortium/pyam/tree/master/doc/source/_examples.
+#
+# Make sure to place the file in the same folder as this script/notebook.
+
+import pyam
+import plotly
+df = pyam.IamDataFrame('sankey_data.csv')
+df
+
+###############################
+# A simple Sankey diagram
+# ***********************
+#
+# We show a Sankey diagram of a subset of the energy system
+# in the 'CD-LINKS_NPi2020_1000' scenario
+# implemented by the 'REMIND-MAgPIE 1.7-3.0' model.
+
+sankey_mapping = {
+    'Primary Energy|Coal': ('Coal Mining', 'Coal Trade'),
+    'Primary Energy|Gas': ('Natural Gas Extraction', 'Gas Network'),
+    'Secondary Energy|Electricity|Non-Biomass Renewables':
+        ('Non-Biomass Renewables', 'Electricity Grid'),
+    'Secondary Energy|Electricity|Nuclear': ('Nuclear', 'Electricity Grid'),
+    'Secondary Energy|Electricity|Coal': ('Coal Trade', 'Electricity Grid'),
+    'Secondary Energy|Electricity|Gas': ('Gas Network', 'Electricity Grid'),
+    'Final Energy|Electricity': ('Electricity Grid', 'Electricity Demand'),
+    'Final Energy|Solids|Coal': ('Coal Trade', 'Non-Electricity Coal Demand'),
+    'Final Energy|Gases': ('Gas Network', 'Gas Demand'),
+}
+
+fig = df.filter(year=2050).sankey(mapping=sankey_mapping)
+plotly.io.show(fig)
