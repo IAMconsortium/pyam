759c67743fd0be430bd893be088dc54c52c7b532
Daniel Huppmann <dh@dergelbesalon.at>
2021-02-17 18:12:22 +0100

    Update the data to guard against future changes

--- tests/test_datareader.py
+++ tests/test_datareader.py
@@ -15,8 +15,8 @@ except ConnectionError:
 WB_REASON = 'World Bank API unavailable'
 
 WB_DF = pd.DataFrame([
-    ['foo', 'WDI', 'Canada', 'GDP', 'n/a', 39473.4, 40637.4, 42266.5],
-    ['foo', 'WDI', 'Mexico', 'GDP', 'n/a', 17288.6, 17720.0, 17874.0],
+    ['foo', 'WDI', 'Canada', 'GDP', 'n/a', 39356.9,	40517.5, 42141.8],
+    ['foo', 'WDI', 'Mexico', 'GDP', 'n/a', 17262.3, 17693.0, 17846.0],
     ['foo', 'WDI', 'United States', 'GDP', 'n/a', 51569.8, 53035.7, 54395.4]
 ], columns=IAMC_IDX + [2003, 2004, 2005])
 
