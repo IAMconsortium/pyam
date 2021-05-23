384ce54c6f171e6294ebf51d2afe051abfdeb903
Daniel Huppmann <dh@dergelbesalon.at>
2021-05-23 07:55:04 +0200

    Add a sanity check (and test) for using non-sensical args in a binary op

--- pyam/_ops.py
+++ pyam/_ops.py
@@ -31,6 +31,9 @@ def _op_data(df, method_args, name, method, axis, **kwds):
         kwds[key] = _get_values(df, axis, value, cols)
 
     _value = method(*_args, **kwds)
+    if not isinstance(_value, pd.Series):
+        msg = "The value returned by the method cannot be cast to an IamDataFrame"
+        raise ValueError(f"{msg}: {_value}")
     _value.index = append_index_level(_value.index, codes=0, level=name, name=axis)
 
     return _value
