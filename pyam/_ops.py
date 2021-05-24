bf2b1a306a76c16d39be1bb7bc10d38596969710
Daniel Huppmann <dh@dergelbesalon.at>
2021-05-24 07:44:23 +0200

    Implement `fillna` operation in internal `_op_data()`

--- pyam/_ops.py
+++ pyam/_ops.py
@@ -43,21 +43,43 @@ def _op_data(df, name, method, axis, fillna=None, args=(), **kwds):
         raise ValueError(f"Unknown method: {method}")
 
     cols = df._data.index.names.difference([axis])
-    _args = [_get_values(df, axis, value, cols) for value in args]
 
-    for key, value in kwds.items():
-        kwds[key] = _get_values(df, axis, value, cols)
+    # replace args and and kwds with values of `df._data` if applicable
+    _args, _data_args = [None] * len(args), [False] * len(args)
+    for i, value in enumerate(args):
+        _args[i], _data_args[i] = _get_values(df, axis, value, cols, f"_arg{i}")
 
+    _data_kwds = {}
+    for i, (key, value) in enumerate(kwds.items()):
+        kwds[key], _data_kwds[key] = _get_values(df, axis, value, cols, key)
+
+    # merge all args and kwds that are based on `df._data` to apply fillna
+    if fillna:
+        _data_cols = [_args[i] for i, is_data in enumerate(_data_args) if is_data]
+        _data_cols += [kwds[key] for key, is_data in _data_kwds.items() if is_data]
+        _data = pd.merge(*_data_cols, how="outer", left_index=True, right_index=True)
+
+        _data.fillna(fillna, inplace=True)
+
+        for i, is_data in enumerate(_data_args):
+            if is_data:
+                _args[i] = _data[f"_arg{i}"]
+        for key, is_data in _data_kwds.items():
+            if is_data:
+                kwds[key] = _data[key]
+
+    # apply method and check that returned object is valid
     _value = method(*_args, **kwds)
     if not isinstance(_value, pd.Series):
         msg = f"Value returned by `{method.__name__}` cannot be cast to an IamDataFrame"
         raise ValueError(f"{msg}: {_value}")
+
+    # insert the index level and reset the series name
     _value.index = append_index_level(_value.index, codes=0, level=name, name=axis)
+    return _value.rename(index=None)
 
-    return _value
 
-
-def _get_values(df, axis, value, cols):
+def _get_values(df, axis, value, cols, name):
     """Return grouped data if value is in axis. Otherwise return value.
 
     Parameters
@@ -70,6 +92,8 @@ def _get_values(df, axis, value, cols):
         Either str or list of str in axis or anything else.
     cols : list
         Columns in df that are not `axis`.
+    name : str
+        Name of the returned pd.Series.
 
     Returns
     -------
@@ -77,6 +101,7 @@ def _get_values(df, axis, value, cols):
 
     """
     if any(v in get_index_levels(df._data, axis) for v in to_list(value)):
-        return df.filter(**{axis: value})._data.groupby(cols).sum()
+        _data = df.filter(**{axis: value})._data.groupby(cols).sum().rename(index=name)
+        return _data, True
     else:
-        return value
+        return value, False
