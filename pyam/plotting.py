5594b170c30edc418c6a1652c3e6c63e33c75a79
Matthew Gidden <matthew.gidden@gmail.com>
2021-06-15 22:06:04 +0200

    update ipcc colors for wg1

--- pyam/plotting.py
+++ pyam/plotting.py
@@ -47,7 +47,7 @@ OUTSIDE_LEGEND = {
 PYAM_COLORS = {
     # AR6 colours from https://github.com/IPCC-WG1/colormaps
     # where each file is processed to generate hex values, e.g.:
-    # for liwith open('rcp_cat.txt') as f:
+    # with open('rcp_cat.txt') as f:
     #   for l in f.readlines():
     #     rgb = np.array([int(x) for x in l.strip().split()]) / 256
     #     print(matplotlib.colors.rgb2hex(rgb))
@@ -56,15 +56,15 @@ PYAM_COLORS = {
     "AR6-SSP3": "#f11111",
     "AR6-SSP4": "#e78731",
     "AR6-SSP5": "#8036a7",
-    "AR6-SSP1-1.9": "#1e9583",
-    "AR6-SSP1-2.6": "#1d3354",
-    "AR6-SSP2-4.5": "#e9dc3d",
-    "AR6-SSP3-7.0": "#f11111",
-    "AR6-SSP3-LowNTCF": "#f11111",
+    "AR6-SSP1-1.9": "#00acce",
+    "AR6-SSP1-2.6": "#173c66",
+    "AR6-SSP2-4.5": "#f69320",
+    "AR6-SSP3-7.0": "#e61d25",
+    "AR6-SSP3-LowNTCF": "#e61d25",
     "AR6-SSP4-3.4": "#63bce4",
     "AR6-SSP4-6.0": "#e78731",
     "AR6-SSP5-3.4-OS": "#996dc8",
-    "AR6-SSP5-8.5": "#830b22",
+    "AR6-SSP5-8.5": "#941b1e",
     "AR6-RCP-2.6": "#980002",
     "AR6-RCP-4.5": "#c37900",
     "AR6-RCP-6.0": "#709fcc",
