7ad454cbfa052820e43447ba1510d0757b932243
maartenbrinkerink <65602545+maartenbrinkerink@users.noreply.github.com>
2021-07-26 07:44:53 +0100

    Add an example with multiple axes representing different units (#565)
    Co-authored-by: Daniel Huppmann <dh@dergelbesalon.at>

--- doc/source/examples/plot_secondary_axis.py
+++ doc/source/examples/plot_secondary_axis.py
@@ -24,10 +24,45 @@ df = pyam.IamDataFrame("tutorial_data.csv")
 df
 
 ###############################
+# Create a figure with different units on axes
+# ************************************************
+#
+# To create a chart with multiple axes, we directly use the **matplotlib** package
+# and start with a subplot consisting of a figure canvas and
+# an :class:`Axes <matplotlib.axes.Axes>` object, which contains the figure elements.
+#
+# First, we generate a simple line chart with temperature increase in °C
+# for one scenario and multiple models.
+# We now tell **pyam** to specifically use the :code:`ax` instance for the plot.
+#
+# Then we create a second axis :code:`ax2` using :meth:`Axes.secondary_yaxis() <matplotlib.axes.Axes.secondary_yaxis>`
+# while converting temperature increase in °C from the original axis :code:`ax`
+# to temperature increase in °F and add it to the plot.
+
+fig, ax = plt.subplots()
+
+args = dict(
+    scenario="CD-LINKS_NPi2020_1000",
+    region="World",
+)
+
+temperature = "AR5 climate diagnostics|Temperature|Global Mean|MAGICC6|MED"
+data_temperature = df.filter(**args, variable=temperature)
+data_temperature.plot(ax=ax, title=None, legend=False)
+
+ax2 = ax.secondary_yaxis('right', functions=(lambda x: x * 1.8, lambda x: x / 1.8))
+ax2.set_ylabel("°F")
+
+ax.set_title("Temperature change relative to pre-industrial")
+
+plt.tight_layout()
+plt.show()
+
+###############################
 # Create a composed figure from several plot types
 # ************************************************
 #
-# To create a composed chart, we directly use the **matplotlib** package
+# To create a composed chart, we again use the **matplotlib** package
 # and start with a subplot consisting of a figure canvas and
 # an :class:`Axes <matplotlib.axes.Axes>` object, which contains the figure elements.
 #
