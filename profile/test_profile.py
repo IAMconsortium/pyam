a21c058950688b8ef665a5a6c26faee16ec913e0
Daniel Huppmann <dh@dergelbesalon.at>
2021-09-28 22:57:59 +0200

    Add conftest to run tests on all xlsx files in `data`

--- profile/test_profile.py
+++ profile/test_profile.py
@@ -1,18 +1,5 @@
-import pytest
-from pyam import IamDataFrame, IAMC_IDX
-import pandas as pd
+from pyam import IamDataFrame
 
 
-TEST_DF = pd.DataFrame(
-    [
-        ["model_a", "scen_a", "World", "Primary Energy", "EJ/yr", 1, 6.0],
-        ["model_a", "scen_a", "World", "Primary Energy|Coal", "EJ/yr", 0.5, 3],
-        ["model_a", "scen_b", "World", "Primary Energy", "EJ/yr", 2, 7],
-    ],
-    columns=IAMC_IDX + [2005, 2010],
-)
-
-
-@pytest.mark.parametrize("data", [TEST_DF])
 def test_init(data):
     IamDataFrame(data)
