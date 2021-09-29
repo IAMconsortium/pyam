cdcfea83fc415663c61b0e95210b3061d90b1e55
Daniel Huppmann <dh@dergelbesalon.at>
2021-09-29 10:21:09 +0200

    Appease stickler again

--- profile/profile_report.py
+++ profile/profile_report.py
@@ -7,8 +7,8 @@ def main():
 
     print(
         pd.read_sql_query(
-            "SELECT item_path, item_variant, total_time, cpu_usage, mem_usage FROM test_metrics "
-            "WHERE session_h = ("
+            "SELECT item_path, item_variant, total_time, cpu_usage, mem_usage "
+            "FROM test_metrics WHERE session_h = ("
             "  SELECT session_h FROM test_sessions "
             "  WHERE run_date = (SELECT MAX(run_date) FROM test_sessions)"
             ")",
