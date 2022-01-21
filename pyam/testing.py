3c72079e8e9630e0ce9f3da6a8584a65afd32a4c
Daniel Huppmann <dh@dergelbesalon.at>
2022-01-21 08:50:18 +0100

    Add the fix to `assert_iamframe_equal()`

--- pyam/testing.py
+++ pyam/testing.py
@@ -8,4 +8,10 @@ def assert_iamframe_equal(a, b, **assert_kwargs):
         msg = "IamDataFrame.data are different: \n {}"
         raise AssertionError(msg.format(diff.head()))
 
-    pdt.assert_frame_equal(a.meta, b.meta, check_dtype=False, check_like=True)
+    pdt.assert_frame_equal(
+        _drop_nan_col(a.meta), _drop_nan_col(b.meta), check_dtype=False, check_like=True
+    )
+
+
+def _drop_nan_col(df):
+    return df.dropna(axis="columns", how="all")
