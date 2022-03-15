0b7993b943b2a87db51d5827462d2daff7341d7a
Jonas Hörsch <jonas.hoersch@climateanalytics.org>
2022-03-15 20:43:32 +0100

    Reactivate test fixed #647.
    Co-authored-by: Daniel Huppmann <dh@dergelbesalon.at>

--- tests/test_unfccc.py
+++ tests/test_unfccc.py
@@ -11,7 +11,7 @@ UNFCCC_DF = pd.DataFrame(
 INDEX_ARGS = dict(model="UNFCCC", scenario="Data Inventory")
 
 
-def _test_unfccc_tier1():
+def test_unfccc_tier1():
     # test that UNFCCC API returns expected data and units
     exp = IamDataFrame(
         UNFCCC_DF,
