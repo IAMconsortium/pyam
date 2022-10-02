893fd2f892346a19c8e564e9d0c2526884c633b5
Daniel Huppmann <dh@dergelbesalon.at>
2022-10-02 13:55:57 +0200

    Hardcoding line-terminator

--- tests/test_io.py
+++ tests/test_io.py
@@ -66,7 +66,7 @@ def test_io_csv_none(test_df_year):
         "model_a,scen_a,World,Primary Energy|Coal,EJ/yr,0.5,3.0\n"
         "model_a,scen_b,World,Primary Energy,EJ/yr,2.0,7.0\n"
     )
-    assert test_df_year.to_csv() == exp
+    assert test_df_year.to_csv(lineterminator="\n") == exp
 
 
 @pytest.mark.parametrize(
