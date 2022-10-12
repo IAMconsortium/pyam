ab791c8c95a142214cc0de1ebdb7baee6fa6d506
Matthew Gidden <matthew.gidden@gmail.com>
2022-10-12 16:14:20 +0200

    apply black to test_feature_quantile.py

--- tests/test_feature_quantiles.py
+++ tests/test_feature_quantiles.py
@@ -7,24 +7,27 @@ import pandas as pd
 def test_qunatile_one_variable(test_pd_df):
     """Tests interquartile range of standard test df
 
-    Because it is only two datapoints, the only 'new' computation 
+    Because it is only two datapoints, the only 'new' computation
     is the median
     """
     df = IamDataFrame(test_pd_df)
     quantiles = (0.25, 0.5, 0.75)
-    obs = df.filter(variable='Primary Energy').quantiles(quantiles)
+    obs = df.filter(variable="Primary Energy").quantiles(quantiles)
     exp = IamDataFrame(
-        pd.DataFrame({
-            'scenario': [f'quantile_{q}' for q in quantiles],
-            '2005': [1, (1. + 2) / 2, 2],
-            '2010': [6, (6 + 7) / 2, 7],
-            }),
+        pd.DataFrame(
+            {
+                "scenario": [f"quantile_{q}" for q in quantiles],
+                "2005": [1, (1.0 + 2) / 2, 2],
+                "2010": [6, (6 + 7) / 2, 7],
+            }
+        ),
         model="unweighted",
         region="World",
         variable="Primary Energy",
         unit="EJ/yr",
     )
     assert_iamframe_equal(exp, obs)
+
 
 def test_quantile_multiple_variables(test_pd_df):
     df = IamDataFrame(test_pd_df)
