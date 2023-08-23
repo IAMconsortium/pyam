91bb0b001286ddfc945b4e10f82e87d771641946
Daniel Huppmann <dh@dergelbesalon.at>
2023-08-23 21:03:56 +0200

    Harmonize logging of ixmp4 and pyam in Jupyter notebooks

--- pyam/__init__.py
+++ pyam/__init__.py
@@ -15,15 +15,12 @@ from pyam.core import (
     validate,
 )
 from pyam.statistics import Statistics
-from pyam.logging import *
 from pyam.iiasa import read_iiasa, lazy_read_iiasa  # noqa: F401
 from pyam.datareader import read_worldbank  # noqa: F401
 from pyam.unfccc import read_unfccc  # noqa: F401
 from pyam.testing import assert_iamframe_equal  # noqa: F401
 from pyam.run_control import run_control  # noqa: F401
 from pyam.utils import IAMC_IDX  # noqa: F401
-
-from pyam.logging import defer_logging_config
 
 logger = logging.getLogger(__name__)
 
@@ -42,19 +39,19 @@ try:
     from ipykernel.zmqshell import ZMQInteractiveShell
     from IPython import get_ipython
 
+    # harmonize formatting of ixmp4 and pyam logging
+    ixmp4_logger = logging.getLogger("ixmp4")
+    ixmp4_logger.removeHandler(ixmp4_logger.handlers[0])
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(name)s - %(levelname)s: %(message)s"))
+
+    for _logger in [logger, ixmp4_logger].
+        _logger.addHandler(handler)
+
+    # deactivate in-cell scrolling in a Jupyter notebook
     shell = get_ipython()
     if isinstance(shell, ZMQInteractiveShell):
-        # set up basic logging if running in a notebook
-        log_msg = "Running in a notebook, setting up a basic logging at level INFO"
-
-        defer_logging_config(
-            logger,
-            log_msg,
-            level="INFO",
-            format="%(name)s - %(levelname)s: %(message)s",
-        )
-
-        # deactivate in-cell scrolling in a Jupyter notebook
         shell.run_cell_magic(
             "javascript",
             "",
--- pyam/logging.py
+++ pyam/logging.py
@@ -1,17 +1,18 @@
 from contextlib import contextmanager
-import logging
+from logging import *
+
 import pandas as pd
 import warnings
 
 
-logger = logging.getLogger(__name__)
+logger = getLogger(__name__)
 
 
 @contextmanager
 def adjust_log_level(logger="pyam", level="ERROR"):
     """Context manager to change log level"""
     if isinstance(logger, str):
-        logger = logging.getLogger(logger)
+        logger = getLogger(logger)
     old_level = logger.getEffectiveLevel()
     logger.setLevel(level)
     yield
@@ -32,52 +33,3 @@ def raise_data_error(msg, data):
     data = data.drop_duplicates()
     msg = f"{msg}:\n{data.head()}" + ("\n..." if len(data) > 5 else "")
     raise ValueError(msg)
-
-
-class ConfigPseudoHandler(logging.Handler):
-    """Pseudo logging handler to defer configuring logging until the first message
-
-    Registers itself as a handler for the provided logger and temporarily
-    sets the logger as sensitive to INFO messages. Upon receival of the first
-    message (of at least INFO level), it configures logging with the provided
-    `config_kwargs` and prints `log_msg`
-
-    Parameters
-    ----------
-    logger : logging.Logger
-        Logger to listen for the first message
-    log_msg : str, optional
-        Message to print once logging is configured, by default None
-    **config_kwargs
-        Arguments to pass on to logging.basicConfig
-    """
-
-    def __init__(self, logger, log_msg=None, **config_kwargs):
-        super().__init__()
-
-        self.logger = logger
-        self.log_msg = log_msg
-        self.config_kwargs = config_kwargs
-
-        self.logger.addHandler(self)
-
-        # temporarily set the logging level to a non-standard value,
-        # slightly below logging.INFO == 20 and use that as a sentinel
-        # to switch back to logging.NOTSET later
-        self.logger.setLevel(19)
-
-    def emit(self, record):
-        self.logger.removeHandler(self)
-
-        if self.logger.level == 19:
-            self.logger.setLevel(logging.NOTSET)
-
-        if not self.logger.root.hasHandlers():
-            logging.basicConfig(**self.config_kwargs)
-
-            if self.log_msg is not None:
-                self.logger.info(self.log_msg)
-
-
-# Give the Handler a function like alias
-defer_logging_config = ConfigPseudoHandler
