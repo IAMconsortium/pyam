7561ffdd2990269a194fd35e462d774cfd386cb2
Daniel Huppmann <dh@dergelbesalon.at>
2023-09-18 13:48:32 +0200

    Reinsert plotting gallery

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ language = "English"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "gallery"]
+exclude_patterns = ["_build"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
