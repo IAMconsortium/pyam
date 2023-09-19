5f938a11f9b2c212a9038c81bf775c06e1ca2dda
Daniel Huppmann <dh@dergelbesalon.at>
2023-09-19 07:20:51 +0200

    Specifically exclude gallery-notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ language = "English"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build"]
+exclude_patterns = ["_build", "gallery/plot_*.ipynb"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
