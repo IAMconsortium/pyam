401207e80102325dcdb452e79a892d734a51684a
Daniel Huppmann <dh@dergelbesalon.at>
2023-11-15 11:04:41 +0100

    Make black

--- pyam/ixmp4.py
+++ pyam/ixmp4.py
@@ -3,6 +3,7 @@ import ixmp4
 from ixmp4.core.region import RegionModel
 from ixmp4.core.unit import UnitModel
 
+
 def write_to_ixmp4(df, platform: ixmp4.Platform):
     """Save all scenarios as new default runs in an ixmp4 platform database instance
 
@@ -21,13 +22,13 @@ def write_to_ixmp4(df, platform: ixmp4.Platform):
     # quickfix: ensure that units and regions exist before writing
     for dimension, values, model in [
         ("regions", df.region, RegionModel),
-        ("units", df.unit, UnitModel)
+        ("units", df.unit, UnitModel),
     ]:
         platform_values = platform.__getattribute__(dimension).tabulate().name.values
         if missing := [i for i in values if i not in platform_values]:
             raise model.NotFound(
-                ", ".join(missing) +
-                f". Use `Platform.{dimension}.create()` to add the missing {dimension}."
+                ", ".join(missing)
+                + f". Use `Platform.{dimension}.create()` to add the missing {dimension}."
             )
 
     for model, scenario in df.index:
