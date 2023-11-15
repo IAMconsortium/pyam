391aee6cc50517ee824355a4e0b1a2fa58ce988b
Daniel Huppmann <dh@dergelbesalon.at>
2023-11-15 10:28:39 +0100

    Add a smoke test

--- tests/test_ixmp4.py
+++ tests/test_ixmp4.py
@@ -18,3 +18,11 @@ def test_to_ixmp4_missing_unit_raises(test_df_year):
     platform.regions.create(name="World", hierarchy="common")
     with pytest.raises(UnitModel.NotFound, match="EJ/yr. Use `Platform"):
         test_df_year.to_ixmp4(platform=platform)
+
+
+def test_ixmp4_integration(test_df):
+    """Write an IamDataFrame to the platform"""
+    platform = Platform(_backend=SqliteTestBackend())
+    platform.regions.create(name="World", hierarchy="common")
+    platform.units.create(name="EJ/yr")
+    test_df.to_ixmp4(platform=platform)
