4ca56175d826da7d39269ff30e130fe9a93da1bb
Daniel Huppmann <dh@dergelbesalon.at>
2024-03-01 12:46:13 +0100

    Separate out tests between year and other time domain

--- tests/test_ixmp4.py
+++ tests/test_ixmp4.py
@@ -23,8 +23,8 @@ def test_to_ixmp4_missing_unit_raises(test_df_year):
         test_df_year.to_ixmp4(platform=platform)
 
 
-def test_ixmp4_integration(test_df):
-    """Write an IamDataFrame to the platform"""
+def test_ixmp4_time_not_implemented(test_df):
+    """Writing an IamDataFrame with datetime-data is not implemented"""
     platform = Platform(_backend=SqliteTestBackend())
     platform.regions.create(name="World", hierarchy="common")
     platform.units.create(name="EJ/yr")
@@ -32,21 +32,29 @@ def test_ixmp4_integration(test_df):
     if test_df.time_domain != "year":
         with pytest.raises(NotImplementedError):
             test_df.to_ixmp4(platform=platform)
-    else:
-        # test writing to platform
-        test_df.to_ixmp4(platform=platform)
+        # pass
 
-        # read only default scenarios (runs) - version number added as meta indicator
-        obs = read_ixmp4(platform=platform)
-        exp = test_df.copy()
-        exp.set_meta(1, "version")  # add version number added from ixmp4
-        pyam.assert_iamframe_equal(exp, obs)
 
-        # read all scenarios (runs) - version number used as additional index dimension
-        obs = read_ixmp4(platform=platform, default_only=False)
-        data = test_df.data
-        data["version"] = 1
-        meta = test_df.meta.reset_index()
-        meta["version"] = 1
-        exp = pyam.IamDataFrame(data, meta=meta, index=["model", "scenario", "version"])
-        pyam.assert_iamframe_equal(exp, obs)
+def test_ixmp4_integration(test_df_year):
+    """Write an IamDataFrame to the platform"""
+    platform = Platform(_backend=SqliteTestBackend())
+    platform.regions.create(name="World", hierarchy="common")
+    platform.units.create(name="EJ/yr")
+
+    # test writing to platform
+    test_df_year.to_ixmp4(platform=platform)
+
+    # read only default scenarios (runs) - version number added as meta indicator
+    obs = read_ixmp4(platform=platform)
+    exp = test_df_year.copy()
+    exp.set_meta(1, "version")  # add version number added from ixmp4
+    pyam.assert_iamframe_equal(exp, obs)
+
+    # read all scenarios (runs) - version number used as additional index dimension
+    obs = read_ixmp4(platform=platform, default_only=False)
+    data = test_df_year.data
+    data["version"] = 1
+    meta = test_df_year.meta.reset_index()
+    meta["version"] = 1
+    exp = pyam.IamDataFrame(data, meta=meta, index=["model", "scenario", "version"])
+    pyam.assert_iamframe_equal(exp, obs)
