221f1ce44bb5474a86ebb8e4303074fc20f88979
Daniel Huppmann <dh@dergelbesalon.at>
2024-03-06 22:31:57 +0100

    Implement more consistent logging behavior with ixmp4

--- pyam/__init__.py
+++ pyam/__init__.py
@@ -24,9 +24,6 @@ from pyam.testing import assert_iamframe_equal  # noqa: F401
 from pyam.unfccc import read_unfccc  # noqa: F401
 from pyam.utils import IAMC_IDX  # noqa: F401
 
-logger = logging.getLogger(__name__)
-
-# get version number either from git (preferred) or metadata
 try:
     __version__ = get_version(root=Path(__file__).parents[1])
 except LookupError:
@@ -36,31 +33,6 @@ except LookupError:
     except PackageNotFoundError:
         __version__ = version("pyam")
 
-# special handling in Jupyter notebooks
-try:
-    from ipykernel.zmqshell import ZMQInteractiveShell
-    from IPython import get_ipython
 
-    shell = get_ipython()
-    if isinstance(shell, ZMQInteractiveShell):
-        # harmonize formatting of ixmp4 and pyam logging
-        ixmp4_logger = logging.getLogger("ixmp4")
-        ixmp4_logger.removeHandler(ixmp4_logger.handlers[0])
-
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter("%(name)s - %(levelname)s: %(message)s"))
-
-        for _logger in [logger, ixmp4_logger]:
-            _logger.addHandler(handler)
-
-        # deactivate in-cell scrolling in a Jupyter notebook
-        shell.run_cell_magic(
-            "javascript",
-            "",
-            "if (typeof IPython !== 'undefined') "
-            "{ IPython.OutputArea.prototype._should_scroll = function(lines)"
-            "{ return false; }}",
-        )
-
-except Exception:
-    pass
+# Set up logging overriding the ixmp4 logging configuration
+logging.configure_logging()
