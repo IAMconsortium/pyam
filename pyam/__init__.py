3deeabd50e70c8736792d2ad5eeeec61975675f1
Daniel Huppmann <dh@dergelbesalon.at>
2024-03-09 09:36:11 +0100

    Refactor initialiization

--- pyam/__init__.py
+++ pyam/__init__.py
@@ -1,6 +1,4 @@
-import logging
 from importlib.metadata import PackageNotFoundError, version
-from pathlib import Path
 
 from pyam.core import (
     IamDataFrame,
@@ -13,14 +11,15 @@ from pyam.core import (
     require_variable,
     validate,
 )
-from pyam.datareader import read_worldbank  # noqa: F401  # noqa: F401
 from pyam.iiasa import lazy_read_iiasa, read_iiasa  # noqa: F401  # noqa: F401
 from pyam.ixmp4 import read_ixmp4  # noqa: F401
+from pyam.logging import configure_logging  # noqa: F401
 from pyam.run_control import run_control  # noqa: F401
-from pyam.statistics import Statistics
+from pyam.statistics import Statistics  # noqa: F401
 from pyam.testing import assert_iamframe_equal  # noqa: F401
 from pyam.unfccc import read_unfccc  # noqa: F401
 from pyam.utils import IAMC_IDX  # noqa: F401
+from pyam.worldbank import read_worldbank  # noqa: F401
 
 try:
     __version__ = version("pyam-iamc")
@@ -28,6 +27,5 @@ try:
 except PackageNotFoundError:
     __version__ = version("pyam")
 
-
 # Set up logging consistent with the ixmp4 "production" logging configuration
-logging.configure_logging()
+configure_logging()
