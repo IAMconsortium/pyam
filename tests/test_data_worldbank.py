fba773fc2423814c292ce7b97e8d753f7ab6d856
Daniel Huppmann <dh@dergelbesalon.at>
2024-06-04 12:11:47 +0200

    Update values for World Bank integration test

--- tests/test_data_worldbank.py
+++ tests/test_data_worldbank.py
@@ -21,8 +21,8 @@ WB_REASON = "World Bank API unavailable"
 
 WB_DF = pd.DataFrame(
     [
-        ["foo", "WDI", "Canada", "GDP", "n/a", 42793.1, 43704.4, 44680.8],
-        ["foo", "WDI", "Mexico", "GDP", "n/a", 18634.9, 19017.8, 19144.0],
+        ["foo", "WDI", "Canada", "GDP", "n/a", 49231.9, 50283.0, 51409.4],
+        ["foo", "WDI", "Mexico", "GDP", "n/a", 20065.3, 20477.6, 19144.0],
         ["foo", "WDI", "United States", "GDP", "n/a", 51569.8, 53035.7, 54395.4],
     ],
     columns=IAMC_IDX + [2003, 2004, 2005],
