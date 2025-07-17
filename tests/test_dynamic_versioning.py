cbbd8bfab57a4cb7e4aa33854bad5a5a29d39cef
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2025-07-17 14:32:47 +0200

    Fix test for GitHub

--- tests/test_dynamic_versioning.py
+++ tests/test_dynamic_versioning.py
@@ -3,7 +3,4 @@ import subprocess
 
 def test_dynamic_versioning():
     obs = subprocess.run(["poetry", "version"], capture_output=True)
-    assert (
-        "0.0.0"
-        not in obs.stdout.decode(encoding="utf-8").strip().split("pyam-iamc ")[1]
-    )
+    assert obs.stdout.decode(encoding="utf-8").strip().split("pyam-iamc ")[1] != "0.0.0"
