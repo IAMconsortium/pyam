f43d170e409cee88a06841e0ff54f232526e4775
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2025-09-10 13:29:13 +0200

    Add test for ignoring meta in IamDataFrame comparison

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from pyam.testing import assert_iamframe_equal
 
@@ -10,3 +11,18 @@ def test_equal_meta_nan_col(test_df_year):
     df.set_meta(meta=np.nan, name="nan-column")  # add a column of np.nan's to `meta`
 
     assert_iamframe_equal(test_df_year, df)
+
+
+def test_equal_meta_different(test_df_year):
+    """Test with different meta values"""
+    # https://github.com/IAMconsortium/pyam/issues/515
+    df = test_df_year.copy()
+    df.set_meta(
+        meta="this will be ignored", name="meta_difference"
+    )  # add a column of np.nan's to `meta`
+
+    # assert that ignoring meta does not raise an error
+    assert_iamframe_equal(test_df_year, df, ignore_meta=True)
+    # assert that not ignoring meta does raise an error
+    with pytest.raises(AssertionError):
+        assert_iamframe_equal(test_df_year, df, ignore_meta=False)
