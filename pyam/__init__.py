4e1cc0527fcc79bb1b4af747cea8fc67737cd7ed
Daniel Huppmann <dh@dergelbesalon.at>
2025-09-23 10:45:45 +0200

    Implement interactive-logging-setup similar to ixmp4

--- pyam/__init__.py
+++ pyam/__init__.py
@@ -1,5 +1,9 @@
 import logging
+import sys
 from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
+
+import yaml
 
 from pyam.core import (
     IamDataFrame,
@@ -22,13 +26,25 @@ from pyam.unfccc import read_unfccc
 from pyam.utils import IAMC_IDX
 from pyam.worldbank import read_worldbank
 
+here = Path(__file__).parent
+
+try:
+    __IPYTHON__  # type: ignore
+    _in_ipython_session = True
+except NameError:
+    _in_ipython_session = False
+
+_sys_has_ps1 = hasattr(sys, "ps1")
+
+# Logging is only configured by default when used in an interactive environment.
+# This follows the setup in ixmp4 and nomenclature.
+if _in_ipython_session or _sys_has_ps1:
+    with open(here / "logging.yaml") as file:
+        logging.config.dictConfig(yaml.safe_load(file))
+
 try:
     __version__ = version("pyam-iamc")
 # the pyam package is distributed under different names on pypi and conda
 except PackageNotFoundError:
     __version__ = version("pyam")
 
-# set logger to INFO if not configured before importing the package
-logger = logging.getLogger("pyam")
-if logger.level == logging.NOTSET:
-    logger.setLevel(logging.INFO)
