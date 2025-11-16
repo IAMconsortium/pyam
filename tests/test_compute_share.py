3cdd4688d16087f2a1e601ca4094e729a280bc2a
Daniel Huppmann <dh@dergelbesalon.at>
2025-11-16 07:46:15 +0100

    Remove duplicate test

--- tests/test_compute_share.py
+++ tests/test_compute_share.py
@@ -28,27 +28,6 @@ def test_compute_share_variable(test_df_year, append):
 
 @pytest.mark.parametrize("append", (False, True))
 def test_compute_share_scenario(test_df_year, append):
-    """Check that computing the share works as expected"""
-
-    data = pd.DataFrame(
-        [["World", "Primary Energy|Coal [Share]", "%", 50.0, 50.0]],
-        columns=["region", "variable", "unit", 2005, 2010],
-    )
-    exp = IamDataFrame(data, model="model_a", scenario="scen_a", meta=test_df_year.meta)
-
-    args = ("Primary Energy|Coal", "Primary Energy", "Primary Energy|Coal [Share]")
-    if append:
-        obs = test_df_year.copy()
-        obs.compute.share(*args, append=append)
-        exp = test_df_year.append(exp)
-    else:
-        obs = test_df_year.compute.share(*args)
-
-    assert_iamframe_equal(exp, obs)
-
-
-@pytest.mark.parametrize("append", (False, True))
-def test_compute_share_scenario(test_df_year, append):
     """Check that computing the share works on a custom axis works as expected"""
 
     data = pd.DataFrame(
